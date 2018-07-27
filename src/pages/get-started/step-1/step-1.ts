e08169fc12413611e3b8e8ac83f5a6fd97f7ecf1
chris-caidson <chris@caidson.com>
2018-07-27 17:19:15 -0400

    Implementing Get Started pages.

--- src/pages/get-started/step-1/step-1.ts
+++ src/pages/get-started/step-1/step-1.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
 import { PdfProvider } from '../../../providers/pdf/pdf';
 
 @IonicPage()
@@ -9,7 +9,43 @@ import { PdfProvider } from '../../../providers/pdf/pdf';
 })
 export class Step_1Page {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public pdfProvider: PdfProvider,) {
-  }
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public pdfProvider: PdfProvider,
+    private modalCtrl: ModalController
+  ) { }
 
+  openVideoModal(video: string) {
+    var data: any;
+
+    switch (video) {
+      case "isa-box":
+        data = {
+          name: "Unpacking Your Isagenix System",
+          url:
+            "https://firebasestorage.googleapis.com/v0/b/globalexecteam-17cfa.appspot.com/o/videos%2FUnpacking-Your-Isagenix-System.mp4?alt=media&token=eb1be1d8-3b29-4632-ae92-29676c655423"
+        };
+        break;
+
+      case "shake-day":
+        data = {
+          name: "How to Do a Shake Day",
+          url:
+            "https://firebasestorage.googleapis.com/v0/b/globalexecteam-17cfa.appspot.com/o/videos%2FHow-to-Do-a-Shake-Day.mp4?alt=media&token=8e219566-1d91-4689-aae3-2a5c4bade945"
+        };
+        break;
+
+      case "cleanse-day":
+        data = {
+          name: "How to Do a Cleanse Day",
+          url:
+            "https://firebasestorage.googleapis.com/v0/b/globalexecteam-17cfa.appspot.com/o/videos%2FHow-to-Do-a-Cleanse-Day.mp4?alt=media&token=3a06ca1d-84a7-4270-826c-6de65aaf9d1a"
+        };
+        break;
+    }
+
+    const myModal = this.modalCtrl.create("VideoModalPage", data);
+    myModal.present();
+  }
 }

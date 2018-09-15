1a8e72bb50f1d68bcfc1a9af890efb91e6548889
chris-caidson <chris@caidson.com>
2018-09-15 16:51:06 -0400

    Minor updates.

--- src/pages/get-success/get-fit/get-success-get-fit.ts
+++ src/pages/get-success/get-fit/get-success-get-fit.ts
@@ -37,7 +37,22 @@ export class GetSuccessGetFitPage {
       case "jess":
         data = {
           name: "Setting Yourself Up for Success with Jessica Johnston",
-          url: "https://firebasestorage.googleapis.com/v0/b/globalexecteam-17cfa.appspot.com/o/videos%2Fjessica-johnston.mp4?alt=media&token=985fb81b-3cca-49fa-a033-307f6b25e822"
+          url:
+            "https://firebasestorage.googleapis.com/v0/b/globalexecteam-17cfa.appspot.com/o/videos%2Fjessica-johnston.mp4?alt=media&token=985fb81b-3cca-49fa-a033-307f6b25e822"
+        };
+        break;
+
+      case "laura-biz":
+        data = {
+          name: "Sample Story - Business",
+          url: "https://firebasestorage.googleapis.com/v0/b/globalexecteam-17cfa.appspot.com/o/videos%2Flaura-thompson-biz.mp4?alt=media&token=d5c5d79a-d882-4cb2-b05c-eb0042be404c"
+        };
+        break;
+
+      case "laura-product":
+        data = {
+          name: "Sample Story - Product",
+          url: "https://firebasestorage.googleapis.com/v0/b/globalexecteam-17cfa.appspot.com/o/videos%2Flaura-thompson-product.mp4?alt=media&token=d7bec49e-557d-4c41-895d-346f55b9e434"
         };
         break;
     }

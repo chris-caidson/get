ad868cc044a24da17c8b0ee09a61eee859e5221a
chris-caidson <chris@caidson.com>
2018-09-15 14:03:38 -0400

    Added overview video

--- src/pages/get-success/overview/overview.ts
+++ src/pages/get-success/overview/overview.ts
@@ -21,4 +21,14 @@ export class OverviewPage {
   goForward() {
     this.navCtrl.setRoot("GetSuccessGetFitPage");
   }
+
+  openVideoModal() {
+    const myModal = this.modalCtrl.create("VideoModalPage", {
+      name: "Global Exec Team Website Overview",
+      url:
+        "https://firebasestorage.googleapis.com/v0/b/globalexecteam-17cfa.appspot.com/o/videos%2Fget-website-training.mp4?alt=media&token=6810ed06-aa50-4b1d-97f1-f53d7e3419c5"
+    });
+
+    myModal.present();
+  }
 }
